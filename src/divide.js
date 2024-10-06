c849e70889b8d7cf0ba1ddafce593923f0ed2acf
yvann <yvann.guyonnet@gmail.com>
2024-10-07 01:24:54 +0200

    changes

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,15 @@
 function divide () {
        work
 }
+
+function divide(a, b) {
+       if (a === undefined || b === undefined) {
+           return undefined;
+       }
+       
+       if (typeof a !== 'number' || typeof b !== 'number') {
+           return undefined;
+       }
+       
+       return a / b;
+   }
